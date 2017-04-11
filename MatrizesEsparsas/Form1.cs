75f3d7046a68baf31945bf92dca3b700594f4651
JuliaAlves <Julieta.a.arruda@gmail.com>
2017-04-10 22:18:00 -0300

    k

--- MatrizesEsparsas/Form1.cs
+++ MatrizesEsparsas/Form1.cs
@@ -23,36 +23,45 @@ namespace MatrizesEsparsas
 
         private void btnAbrirArquivo_Click(object sender, EventArgs e)
         {
-            if(dlgAbrirArq.ShowDialog() == DialogResult.OK)
+            if (dlgAbrirArq.ShowDialog() == DialogResult.OK)
             {
                 StreamReader fs = new StreamReader(dlgAbrirArq.FileName);
                 string linha = "";
-                while((linha = fs.ReadLine()) != null)
+                while ((linha = fs.ReadLine()) != null)
                 {
                     lista.QtasLinhas = lista.QtasLinhas + 1;
                     string numero = "";
-                    for(int i=0; i<linha.Length; i++)
+                    for (int i = 0; i < linha.Length; i++)
                     {
-                        if (i+1> lista.QtasColunas)
+                        if (i + 1 > lista.QtasColunas)
                             lista.QtasColunas++;
 
-                        if (linha[i] != ' ')                        
+                        if (linha[i] != ' ')
                             numero += linha[i];
                         else
                         {
                             lista.Inserir(Convert.ToDouble(numero), i + 1, lista.QtasLinhas);
                             numero = "";
-                        }                            
+                        }
                     }
                 }
                 fs.Close();
-
-                for(int i=1; i<=lista.QtasColunas; i++)
-                    dgvMatriz.Columns.Add(i.ToString(), i.ToString());
-                for (int i = 1; i <= lista.QtasLinhas; i++)
-                    dgvMatriz.Rows.Add();
             }
         }
-                
+
+        private void Listar()
+        {
+            for (int i = 1; i <= lista.QtasColunas; i++)
+                dgvMatriz.Columns.Add(i.ToString(), i.ToString());
+
+            string[] linha = new String[lista.QtasColunas];
+            for (int i = 1; i <= lista.QtasLinhas; i++)
+            {
+                for (int w = 1; w <= lista.QtasColunas; i++)
+                    linha[i + 1] = lista.ValorDe(w, i).ToString();
+
+                dgvMatriz.Rows.Add(linha);
+            }
+        }
     }
 }
